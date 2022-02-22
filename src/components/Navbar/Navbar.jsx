c92584d73de7bc3c71a17450caaec59ae7e0f12a
Ariel Cohen <arieliiox@gmail.com>
2022-02-22 21:26:58 +0200

    init2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,35 @@
-import React from "react";
-import pizzaLogo from "../../assets/pizza.png";
+import React, { useState } from "react";
+import pizzaLogo from "../../assets/images/pizza.png";
+import ReorderIcon from "@material-ui/icons/Reorder";
+import "./Navbar.css";
 
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={pizzaLogo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
-        <Link>Home</Link>
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
+import pizzaVideo from "../../assets/videos/pizza.mp4";
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <video
+        autoplay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={pizzaVideo} type="video/mp4" />
+      </video>
+      <div className="headerContainer">
+        <h1>Pizza Place</h1>
+        <p>Just Eat It</p>
+        <Link to="/menu">
+          <button>Oreder Now !</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
