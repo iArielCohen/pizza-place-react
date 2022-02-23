e966e8debbf5c37de4cee46ed3b979d8999fbe62
Ariel Cohen <arieliiox@gmail.com>
2022-02-23 12:51:39 +0200

    s

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 function MenuItem({ image, name, price }) {
+  const handleClick = () => {
+    alert("Thank You For Ordering But Sadlt This Is Just A Demo");
+  };
   return (
-    <div className="menuItem">
+    <div className="menuItem" onClick={handleClick}>
       <div style={{ backgroundImage: `url(${image})` }}> </div>
       <h1> {name} </h1>
       <p> {price} </p>
--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -9,31 +9,31 @@ export const MenuList = [
   {
     name: "Pepperoni Pizza",
     image: Pepperoni,
-    price: 10.99,
+    price: "10.99 $",
   },
   {
-    name: "Margherita Pizza",
+    name: "Goat Cheese Pizza",
     image: Margherita,
-    price: 11.99,
+    price: "11.99 $",
   },
   {
     name: "Special Pizza",
     image: Special,
-    price: 15.49,
+    price: "15.49 $",
   },
   {
     name: "Vegan Pizza",
     image: Vegan,
-    price: 17.99,
+    price: "17.99 $",
   },
   {
     name: "Pineapple Pizza",
     image: Kosher,
-    price: 9.99,
+    price: "9.99 $",
   },
   {
     name: "Very Expensive Pizza",
     image: Expensive,
-    price: 29.99,
+    price: "29.99 $",
   },
 ];
