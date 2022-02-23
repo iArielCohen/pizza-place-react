2b89bb48f878c320464b441d9a657b3f78083212
Ariel Cohen <arieliiox@gmail.com>
2022-02-23 10:41:57 +0200

    g

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import Menu from "./pages/Menu/Menu";
+import About from "./pages/About/About";
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/menu" exact element={<Menu />} />
+          <Route path="/about" exact element={<About />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/MenuList/MenuList.jsx
@@ -0,0 +1,39 @@
+import Pepperoni from "../../assets/images/pepperoni.jpg";
+import Margherita from "../../assets/images/margherita.jpg";
+import Special from "../../assets/images/special.jpg";
+import Vegan from "../../assets/images/vegan.jpg";
+import Kosher from "../../assets/images/kosher.jpg";
+import Expensive from "../../assets/images/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 10.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "Special Pizza",
+    image: Special,
+    price: 15.49,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Kosher,
+    price: 9.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 29.99,
+  },
+];
